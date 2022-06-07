99b00585cb2003529d1a1798de929cd9f06658ff
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-06-07 13:55:30 +0530

    creating user model

---
+++ DB_Schema/Backend/controllers/userController.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router()
+
+router.get("/", async (req, res) => {
+    
+})
+
+router.post("/create", async (req, res) => {
+    
+})
+
+router.get("/:id", async (req, res) => {
+    
+})
+
+router.patch("/:id/edit", async (req, res) => {
+    
+})
+
+router.get("/:id/addresses", async (req, res) => {
+    
+})
+
+router.post("/:id/addresses/create", async (req, res) => {
+    
+})
+
+router.patch("/:id/addresses/:idx/edit", async (req, res) => {
+    
+})
+
+module.exports = router
--- DB_Schema/Backend/models/userModel.js
+++ DB_Schema/Backend/models/userModel.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 
 const UserSchena = new mongoose.Schema({
+    name: { type: String, require: true },
+    age: { type: Number, required: true },
+    mobile_no: {type:Number,required:true,maxlength:10,minlength:10},
+    addresses: []
 
 })
 
