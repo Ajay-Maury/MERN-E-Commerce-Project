aefd332ed5c3fd33062859944225dc307b27c1ea
Ajay-Maury <ajaymaurya6798@gmail.com>
2022-07-08 06:57:48 +0530

    Product detail page

--- Ecommerce Design/Frontend/src/components/products/ProductDetail.jsx
+++ Ecommerce Design/Frontend/src/components/products/ProductDetail.jsx
@@ -9,7 +9,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
-import { Stack } from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
 import { Box } from "@mui/system";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductById } from "../../redux/productByID/action";
@@ -41,11 +41,59 @@ const ProductDetail = () => {
   const { _id, colour, description, image_url, name, price, quantity } =  product;
  
   return (
-    <>
-    {loading && <div>Loading please wait ...</div>}
-    {error && <div>{error}</div>}
-     {!loading && !error && <Card variant="outlined" sx={{ maxWidth: 345, margin: "3% auto" }}>
-        <CardMedia component="img" height="140" image={image_url} alt={name} />
+    <Box>
+      {loading && <div>Loading please wait ...</div>}
+      {error && <div>{error}</div>}
+      {!loading && !error && (
+        <Card variant='outlined' sx={{ maxWidth: 600, margin: "3% auto" }}>
+          <Card sx={{ display: "flex" }}>
+            <CardMedia
+              component='img'
+              sx={{ width: 200 }}
+              image={image_url}
+              alt={name}
+            />
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography component='div' variant='h5'>
+                  {name}
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  color='text.secondary'
+                  component='div'
+                >
+                  {description}
+                </Typography>
+                {/* <Typography variant='body2' color='text.secondary'>
+                  Quantity : {quantity}
+                </Typography> */}
+                <Typography
+                  variant='body2'
+                  color='text.secondary'
+                  sx={{ display: "flex", justifyContent: "space-around" }}
+                >
+                  <span>
+                    <strong>Colour :</strong>
+                    {colour}
+                  </span>
+                  <span>
+                    <strong>Price :</strong>
+                    {price}
+                  </span>
+                </Typography>
+              </CardContent>
+              <CardActions
+                sx={{ display: "flex", justifyContent: "space-around" }}
+              >
+                <Button size='small' onClick={() => handleCart(_id)}>
+                  Add to Cart
+                </Button>
+                <Button size='small'>Buy Now</Button>
+              </CardActions>
+            </Box>
+          </Card>
+          {/* <CardMedia component="img" height="140" image={image_url} alt={name} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {name}
@@ -76,9 +124,10 @@ const ProductDetail = () => {
             Add to Cart
           </Button>
           <Button size="small">Buy Now</Button>
-        </CardActions>
-      </Card>}
-    </>
+        </CardActions> */}
+        </Card>
+      )}
+    </Box>
   );
 };
 
